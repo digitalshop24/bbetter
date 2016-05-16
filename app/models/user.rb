310bac28ffc6a9bd50a34fc273c267b714635cfa
K. Kanyuka <kkan.web@gmail.com>
2016-05-16 19:00:45 +0300

    rolify. cancancan. rails_admin. seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ActiveRecord::Base
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
+
+  rails_admin do
+    edit do
+      fields :email, :password, :roles
+    end
+    show do
+      fields :email, :password, :roles
+    end
+    list do
+      field :email
+    end
+  end
 end
