35ace6f07dda385262cb634d430d9006fbbc01cc
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-06-20 19:18:11 +0300

    payment

--- app/controllers/yandex_kassa_controller.rb
+++ app/controllers/yandex_kassa_controller.rb
@@ -11,6 +11,7 @@ class YandexKassaController < ActionController::Base
     p "------------------------------------------------------------------------"
     check_order = YandexKassa::CheckOrder.new(params)
     p "checked"
+    p check_order.response
     render text: check_order.response
   end
 
