a70612d0db91be2d83568cf3fad5b3aeea389456
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-06-28 23:51:00 +0300

    block dissapear when buy tariff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,5 +9,8 @@ class HomeController < ApplicationController
     @summaries = current_user.summaries
     @tariffs = Tariff.all
     @user_tariff = UserTariff.where(user_id: @user.id).first
+    if @user_tariff.present?
+      @current_tarrif = Tariff.find(@user_tariff.tariff_id)
+    end
   end
 end
