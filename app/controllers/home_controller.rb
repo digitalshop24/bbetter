e90f62e0ba7da2a649c9eb4c86a5ceb3a6c870c7
K. Kanyuka <kkan.web@gmail.com>
2016-06-29 13:35:11 +0300

    profile editting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,19 @@ class HomeController < ApplicationController
   before_action :authenticate_user!, only: :profile
   layout 'home'
   def index
-    @user = User.new
+    @user = current_user || User.new
   end
 
   def profile
     @user = current_user
     @summaries = current_user.summaries if @user
     @tariffs = Tariff.all
+  end
+
+  def edit_profile
+    @user = current_user
+    @user.update(user_params)
+    redirect_to :back
   end
 
   def registration
@@ -18,23 +24,25 @@ class HomeController < ApplicationController
     tariff = nil
     if params[:promocode].present?
       pc = Promocode.available.find_by(code: params[:promocode])
-      @user.errors << "Промокод имеет неверное значение" unless pc
+      @user.errors[:promocode] << "имеет неверное значение" unless pc
       tariff = Tariff.find_by(people_number: 1)
-      @user.errors << "Нету подходящего тарифа" unless tariff
-    end
-    pc.update(activated_at: Time.now, user: @user) if pc
-    UserTariff.create(user: @user, tariff: tariff)
-    sign_in(:user, @user)
-    begin
-      UserMailer.password_email(user, generated_password).deliver_now
-      flash[:info] = 'Пароль был отправлен на почту'
-    rescue => error
-      flash[:info] = "Пароль не был отправлен на почту, потому что #{error.message}"
+      @user.errors[:promocode] << "нету подходящего тарифа" unless tariff
     end
     if @user.save
-      flash[:info] = "Регистрация не было успешной"
+      pc.update(activated_at: Time.now, user: @user) if pc
+      UserTariff.create(user: @user, tariff: tariff)
+      begin
+        UserMailer.password_email(user, generated_password).deliver_now
+        flash[:info] = 'Пароль был отправлен на почту'
+      rescue => error
+        flash[:info] = "Пароль не был отправлен на почту, потому что #{error.message}"
+      end
+      flash[:info] = "Вы успешно зарегистрировались"
+      sign_in(:user, @user)
       redirect_to :profile
     else
+      binding.pry
+      flash[:info] = "Регистрация не было успешной"
       flash[:modal] = 'regModal'
       render 'index'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount API::Root => '/'
   mount GrapeSwaggerRails::Engine => '/apidoc'
-  devise_for :users
+  devise_for :users, controllers: { sessions: "users/sessions" }
   root 'home#index' 
   post '/registration', to: "home#registration", as: :home_registration
+  put '/edit_profile', to: "home#edit_profile", as: :edit_profile
   get '/profile', to: "home#profile", as: :profile
   get '/testcheck', to: "application#testcheck"
   post '/testcheck', to: "yandex_kassa#testcheck"
