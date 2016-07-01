fb6d7d300f2fe641552999e1f0da27b6917dad84
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-07-01 16:12:45 +0300

    fixed types or fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629174919) do
+ActiveRecord::Schema.define(version: 20160701102331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -167,6 +167,8 @@ ActiveRecord::Schema.define(version: 20160629174919) do
     t.integer  "age"
     t.integer  "sex"
     t.text     "motivation"
+    t.string   "moto"
+    t.string   "phone"
   end
 
   add_index "users", ["auth_token"], name: "index_users_on_auth_token", unique: true, using: :btree
