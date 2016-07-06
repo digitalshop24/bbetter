4c56db528e327caf2e7e38b53e68aa7bde5a0866
K. Kanyuka <kkan.web@gmail.com>
2016-07-06 15:23:21 +0300

    feedback to email

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,2 +1,9 @@
 class Feedback < ActiveRecord::Base
+	INFO_EMAIL = ENV['info_email']
+	after_create :send_info_email
+
+	private
+	def send_info_email
+		UserMailer.feedback_info_email(INFO_EMAIL, self).deliver_now
+	end
 end
