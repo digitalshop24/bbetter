815ee39a526414850017d520a72510d522675752
Andrey Fedorovich <a.d.fedorovich@gmail.com>
2016-07-14 21:42:22 +0300

    whyyyy

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -121,7 +121,7 @@ $('[rel="popover"]').popover({
     html: true,
     content: function () {
         var clone = $($(this).data('popover-content')).clone(true).removeClass('hide');
-        return clone;
+        return clone; 
     }
 }).click(function(e) {
     e.preventDefault();
